aaf96ba6425377d1733c17cee8504bf47368426e
SphiweNyoni <165796644+SphiweNyoni@users.noreply.github.com>
2025-10-05 12:46:33 +0200

    feat: migrate footer component to new structure with updated styles and HTML

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { SearchComponent } from './components/search/search.component';
 import { ServiceCategoriesComponent } from './components/service-categories/service-categories.component';
 import { TopRatedProfessionalsComponent } from './components/top-rated-professionals/top-rated-professionals.component';
 import { CtaSectionComponent } from './components/call-to-action/cta-section.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
